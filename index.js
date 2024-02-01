3f2fbc4bcbd3d664ee519109854efbd28b6a31f2
Abdul Owhab <owhab.cse@gmail.com>
2024-02-02 02:30:01 +0600

    return a array as get request data

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  {
+    id: 1,
+    name: "Node JS",
+  },
+  {
+    id: 2,
+    name: "Python Django",
+  },
+  {
+    id: 3,
+    name: "ASP Dot Net",
+  },
+];
+
 const port = process.env.PORT || 4000;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3, 4, 5]);
+  res.send(courses);
 });
 
 app.get("/api/courses/:id", (req, res) => {
