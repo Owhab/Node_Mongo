eecae61e6c3e22c3319e981b6cfe5a9556627752
Abdul Owhab <owhab.cse@gmail.com>
2024-02-02 16:26:46 +0600

    handling delete request and delete a course

--- index.js
+++ index.js
@@ -54,10 +54,21 @@ app.post("/api/courses", (req, res) => {
 
 app.put("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(400).send("Requested course is not found");
+  if (!course) res.status(404).send("Requested course is not found");
   course.name = req.body.name;
   res.send(course);
 });
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("Requested course is not found");
+
+  const index = courses.indexOf(course);
+
+  courses.splice(index, 1);
+  res.send(course);
+});
+
 app.listen(port, () => {
   console.log("Listening on port: ", port);
 });
