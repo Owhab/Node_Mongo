5c34c97572f16aa01b157c0847f0a3dc5bd412a8
Abdul Owhab <owhab.cse@gmail.com>
2024-02-04 00:48:13 +0600

    creating mongoose Schema, Model and Save data to mongodb

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ mongoose
     console.log("Successfully connected to the mongodb local database..")
   )
   .catch((error) => console.log("Failed to connect to the mongodb database.."));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  isPublished: Boolean,
+  Date: { type: Date, default: Date.now },
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function CreateCourse() {
+  const course = new Course({
+    name: "JavaScript Development",
+    author: "Abdul Owhab",
+    tags: ["Frontend", "Backend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+CreateCourse();
