b2bfc73230ba7134ed5e09093ed0468a297bf941
Abdul Owhab <owhab.cse@gmail.com>
2024-02-04 23:05:01 +0600

    working with custom validators in mongoose

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ const courseSchema = new mongoose.Schema({
     enum: ["web", "mobile", "desktop"],
   },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validator: {
+      validate: function (v) {
+        return v && v.length > 0;
+      },
+      message: "A course should have at least one tag.",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
   price: {
