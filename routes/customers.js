277cdc325414ee0f14fa4f0ba6f86feb9c91e3b4
Abdul Owhab <owhab.cse@gmail.com>
2024-02-05 00:25:51 +0600

    created customers get api endpoint

--- routes/customers.js
+++ routes/customers.js
@@ -2,8 +2,27 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World");
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  isGold: Boolean,
+  phone: {
+    type: String,
+    required: true,
+    minlength: 11,
+    maxlength: 11,
+  },
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+router.get("/", async (req, res) => {
+  const customers = await Customer.find();
+  res.send(customers);
 });
 
 module.exports = router;
